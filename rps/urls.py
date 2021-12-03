5585d878c01a237f8702da0a1596c71c97a60d22
bracealround <souravsaha0152@gmail.com>
2021-12-03 12:13:28 +0600

    added the file upload button

--- rps/urls.py
+++ rps/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 # URLconf
 urlpatterns = [
+    path("", views.home_view, name="home"),
     path("students/", views.students_view, name="students"),
     path("teachers/", views.teachers_view, name="teachers"),
     path("results/", views.results_view, name="result"),
     path("results/edit", views.edit_results_view, name="edit-results"),
+    path("results/upload/csv/", views.upload_csv, name='csv_upload'),
 ]
--- rps/views.py
+++ rps/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import logging
+from rps.models import Course, Mark
 
 
 # Function for checking whether user is a student or not
@@ -21,8 +26,8 @@ def is_teacher(user):
 # Create your views here.
 @login_required(login_url="login")
 def home_view(request):
-
-    return render(request, "home.html", {})
+    query_set = Course.objects.all()
+    return render(request, "home.html", {"courses": list(query_set)})
 
 
 @login_required(login_url="login")
@@ -40,10 +45,57 @@ def teachers_view(request):
 @login_required(login_url="login")
 @user_passes_test(is_student, login_url="home")
 def results_view(request):
-    return render(request, "results.html", {})
+
+    query_set = Mark.objects.filter(student__user=request.user)
+
+    return render(request, "results.html", {"marks": list(query_set)})
 
 
 @login_required(login_url="login")
 @user_passes_test(is_teacher, login_url="home")
 def edit_results_view(request):
     return render(request, "edit-results.html", {})
+
+@login_required(login_url="login")
+@user_passes_test(is_teacher, login_url="home")
+def upload_csv(request):
+	data = {}
+	if "GET" == request.method:
+		return render(request, "csv_upload.html", data)
+    # if not GET, then proceed
+	try:
+		csv_file = request.FILES["csv_file"]
+		if not csv_file.name.endswith('.csv'):
+			messages.error(request,'File is not CSV type')
+			return HttpResponseRedirect(reverse("csv_upload"))
+        #if file is too large, return
+		if csv_file.multiple_chunks():
+			messages.error(request,"Uploaded file is too big (%.2f MB)." % (csv_file.size/(1000*1000),))
+			return HttpResponseRedirect(reverse("csv_upload"))
+
+		file_data = csv_file.read().decode("utf-8")		
+
+		lines = file_data.split("\n")
+		#loop over the lines and save them in db. If error , store as string and then display
+		#for line in lines:						
+		#	fields = line.split(",")
+		#	data_dict = {}
+		#	data_dict["name"] = fields[0]
+		#	data_dict["start_date_time"] = fields[1]
+		#	data_dict["end_date_time"] = fields[2]
+		#	data_dict["notes"] = fields[3]
+		#	try:
+		#		form = EventsForm(data_dict)
+		#		if form.is_valid():
+		#			form.save()					
+		#		else:
+		#			logging.getLogger("error_logger").error(form.errors.as_json())												
+		#	except Exception as e:
+		#		logging.getLogger("error_logger").error(repr(e))					
+		#		pass
+
+	except Exception as e:
+		logging.getLogger("error_logger").error("Unable to upload file. "+repr(e))
+		messages.error(request,"Unable to upload file. "+repr(e))
+
+	#return HttpResponseRedirect(reverse("csv_upload"))
