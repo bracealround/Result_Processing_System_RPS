04a3cd74d1a4eb5b3df06d255bbd15c8e6c039d2
bracealround <souravsaha0152@gmail.com>
2021-12-12 03:51:29 +0600

    added institute page

--- rps/urls.py
+++ rps/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path("institute/department/", views.department_view, name="department"),
     path("institute/teacher/", views.institute_teacher_view, name="institute_teacher"),
     path("institute/staff/", views.staff_view, name="institute_staff"),
+    path("institute/info/", views.institute_info_view, name="institute_info"),
     path("institute/students/", views.institute_students_view, name="institute_student"),
     path("students/edit_profile/", views.edit_students_profile_view, name="edit_students_profile"),
     path("teachers/edit_profile/", views.edit_teachers_profile_view,name="edit_teachers_profile"),
